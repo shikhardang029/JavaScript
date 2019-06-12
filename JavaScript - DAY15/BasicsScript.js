5e6a11ee5f3c1aa8ec58357f5538e97a6bdd3687
Shikhar Dang <shikhardang029@gmail.com>
2019-06-12 17:21:12 +0530

    Add files via upload

---
+++ JavaScript - DAY15/BasicsScript.js
@@ -0,0 +1,66 @@
+/* var result = confirm("Do you want to contiue ?");
+alert("Result of confirm is "+result);
+if(result){
+    alert("Yes i want to continue !");
+}
+else{
+    alert("Nope! I dont want to continue !");
+} */
+
+/* var temp = parseInt(prompt("Enter the number"));
+console.log("Prompt is working fine !");
+if(temp < 9)
+alert("Single Digit!");
+else if (temp >=10 && temp<=99)
+alert("Two Digit number");
+else if (temp >=100 && temp<=999)
+alert("Three digit number"); */
+
+/* var num1 = parseInt(prompt("Enter the first number = "));
+var num2 = parseInt(prompt("Enter the second number = "));
+var choice = parseInt(prompt("Enter your choice \n\n\n1) Addition\n2) Substraction\n3) Multiplication\n4) Division"));
+switch (choice) {
+    case 1:
+        alert("Addition = " + (num1 + num2));
+        break;
+    case 2:
+        alert("Substraction = " + (num1 - num2));
+        break;
+    case 3:
+        alert("Multiplication = " + (num1 * num2));
+        break;
+    case 4:
+        alert("Division = " + (num1 / num2));
+        break;
+
+    default:
+        alert("You have entered a wrong input !");
+        break;
+} */
+var day = new Date().getDay();
+switch (day) {
+    case 1:
+        alert("Its Monday!");
+        break;
+    case 2:
+        alert("Its Tuesday!");
+        break;
+    case 3:
+        alert("Its Wednesday!");
+        break;
+    case 4:
+        alert("Its Thrusday!");
+        break;
+    case 4:
+        alert("Its Thrusday!");
+        break;
+    case 4:
+        alert("Its Thrusday!");
+        break;
+    case 4:
+        alert("Its Thrusday!");
+        break;
+
+    default:
+        alert("You have entered a wrong input !");
+        break;
